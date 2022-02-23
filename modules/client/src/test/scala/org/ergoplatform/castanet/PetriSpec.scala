40bacde77050127ab5d986bec01518e75f0360be
iandebeer <ian@zenaptix.com>
2022-02-23 06:49:02 +0200

    File pathe error in castanet depency fixed

--- modules/client/src/test/scala/org/ergoplatform/castanet/PetriSpec.scala
+++ modules/client/src/test/scala/org/ergoplatform/castanet/PetriSpec.scala
@@ -18,9 +18,143 @@ import scala.io.Source
 import scala.quoted.*
 import ee.mn8.castanet.*
 
+import org.ergoplatform.flow.spec.flowspec.Wallet
+import org.ergoplatform.flow.spec.flowspec.Wallet.Box.ErgCondition
+import org.ergoplatform.flow.spec.flowspec.Transaction
+import org.ergoplatform.flow.spec.flowspec.Transaction.InputArrow
+import org.ergoplatform.flow.spec.flowspec.Transaction.SpendingPath
+import org.ergoplatform.flow.spec.flowspec.PetrinetFs2Grpc
+import org.ergoplatform.flow.spec.flowspec.FlowSpec
+import org.ergoplatform.flow.spec.flowspec.FlowSpec.{Parameter => Param}
+import org.ergoplatform.flow.spec.flowspec.Wallet.Box.TokenCondition
+import org.ergoplatform.flow.spec.flowspec.Wallet.Box.Condition
+
 class PetriSpec extends FunSuite {
-  
-  test("build petri net") {
+  def defineSpec() =
+    // The game contract is created by the second player using the funds from the createGameTransaction
+    // The output can be spent by the second player after the end of the game, if the first player fails to provide its secret and answer for the withdrawal
+    // At any time, the winner can withdraw the funds providing the answer and the secret of the first player
+    val gameScript = s""" 
+                        | { // Get inputs from the createGameTransaction, this is the last box in the input list
+                        |   val p2Choice = INPUTS(INPUTS.size-1).R4[Byte].get
+                        |   val p1AnswerHash = INPUTS(INPUTS.size-1).R5[Coll[Byte]].get
+                        |   val player1Pk = INPUTS(INPUTS.size-1).R6[SigmaProp].get
+                        |   val partyPrice = INPUTS(INPUTS.size-1).R7[Long].get
+                        |   val game_end = INPUTS(INPUTS.size-1).R8[Int].get
+                        |   
+                        |   // Get the outputs register
+                        |   val p1Choice = OUTPUTS(0).R4[Byte].get
+                        |   val p1Secret = OUTPUTS(0).R5[Coll[Byte]].get
+                        |   
+                        |   // Compute the winner (the check of the correctness of the winner answer is done later)
+                        |   val p1win = ( p2Choice != p1Choice )
+                        |   
+                        |   sigmaProp (
+                        |      // After the end of the game the second player wins by default
+                        |      // This prevents the first player to block to game by not relealing its answer and secret
+                        |     (player2Pk && HEIGHT > game_end) || 
+                        |       allOf(Coll(
+                        |         // The hash of the first player answer must match
+                        |         blake2b256(p1Secret ++ Coll(p1Choice)) == p1AnswerHash,
+                        |         // The winner can withdraw
+                        |         (player1Pk && p1win )|| (player2Pk && ( p1win == false ))
+                        |       ))
+                        |   )
+                        |  }
+ """.stripMargin
+
+    // The create game contract is created by the first player to engage the game
+    // It allows to cancel the game and get a refund after the end of the game
+    // At any time the funds can be spent by the second player if:
+    //   The funds are protected by the game script
+    //   The output value is more than twice the party price
+    //   The R5 register contains the hash the the answer of the first player
+    //   The R6 register contains public key of the first player
+    //   The party price and game_end are unchanged from the initial contract
+    val createGameScript = s""" 
+      {
+        val gameScriptHash = SELF.R4[Coll[Byte]].get
+        val p1AnswerHash = SELF.R5[Coll[Byte]].get
+
+        sigmaProp (
+          (player1Pk && HEIGHT > game_end) ||
+              allOf(Coll(
+                  player2Pk,
+                  blake2b256(OUTPUTS(0).propositionBytes) == gameScriptHash,
+                  OUTPUTS(0).value >= 2 * partyPrice,
+                  OUTPUTS(0).R5[Coll[Byte]].get == p1AnswerHash,
+                  OUTPUTS(0).R6[SigmaProp].get == player1Pk,
+                  OUTPUTS(0).R7[Long].get == partyPrice,
+                  OUTPUTS(0).R8[Int].get == game_end
+                ))
+        )
+      }
+    """.stripMargin
+
+    val name: String = "Coin Flip Game"
+    val parameters = Seq(
+      Param("playPrice", "Long"),
+      Param("minTxFee", "Long"),
+      Param("playerFunds", "Long"),
+      Param("gameDuration", "Long"),
+      Param("playCount", "Integer"),
+      Param("p1PK", "String"),
+      Param("p2PK", "String")
+    )
+    val wallets: Seq[Wallet] = Seq(
+      Wallet(
+        "player1",
+        "pk",
+        Seq(Wallet.Box("funds", None))
+      ),
+      Wallet(
+        "player2",
+        "pk",
+        Seq(Wallet.Box("funds", None))
+      ),
+      Wallet(
+        "game",
+        "pk",
+        Seq(
+          Wallet.Box(
+            "game",
+            Some(ErgCondition("targetBoxName", "erg_expression")),
+            Seq(TokenCondition("tokeName", "targetBoxName", "expression")),
+            Seq(Condition("targetBoxName", "expression"))
+          )
+        )
+      ),
+      Wallet(
+        "state",
+        "pk",
+        Seq(Wallet.Box("init", None), Wallet.Box("end", None))
+      )
+    )
+    val transactions = Seq(
+      Transaction(
+        name = "provide funds",
+        inputs = Seq(InputArrow("state", "init", Some(SpendingPath("action", "Condition"))))
+      ),
+      Transaction(
+        name = "play game",
+        inputs = Seq(InputArrow("p2Choice", "fromBox", Some(SpendingPath("action", "Condition"))))
+      ),
+      Transaction(
+        name = "create game",
+        inputs = Seq(InputArrow("p2Choice", "fromBox", Some(SpendingPath("action", "Condition"))))
+      ),
+      Transaction(
+        name = "withdraw",
+        inputs = Seq(InputArrow("p2Choice", "fromBox", Some(SpendingPath("action", "Condition"))))
+      )
+    )
+
+    //val boxes: Seq[Wallet.Box] = Seq(new Wallet.Box("",None))
+
+    FlowSpec(name, parameters, wallets, transactions)
+
+  test("build petri net") { 
+
     import Arc._
     val jp1 = """{"id":1,"name":"start","capacity":1}"""
 
@@ -48,9 +182,9 @@ class PetriSpec extends FunSuite {
     // '{(l: String) => println(l)}
 
     val t1: Transition = Transition(6, "splitter", s1, r1)
-/*
+    /*
     val tt1       = t1.asJson.noSpaces
-*/
+     */
 
     val t2: Transition = Transition(7, "joiner", s1, r1)
     val t3: Transition = Transition(8, "continuer", s1, r1)
